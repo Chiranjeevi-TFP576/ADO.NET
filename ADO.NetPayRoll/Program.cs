497ed2148e3f5ba1d54a4e461c6c7f06d747b450
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-02-20 10:15:14 +0530

    [chiru] Create PayRoll Service using DB

---
+++ ADO.NetPayRoll/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ADO.NetPayRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To SQL Program");
+            EmployeeRepository.GetAllEmployee();
+            Console.ReadLine();
+        }
+    }
+}
